2f80641ad5d05a1dc57d59ab81d9fb82da84d8ed
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-03-12 12:15:17 -0500

    Routing-Mini // Kevin Jolley

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
-import Navigation from './components/Navigation';
-import { Home, About, Contact} from './components';
+import { Navigation, Home, About, Contact} from './components'; // You don't need to add index to the end because it's already defaulting to the index - which is what it looks for automatically.
 
 const App = () => (
   <div>
-    <Navigation /> 
+    <Navigation />
+    <Route exact path = "/" component = { Home } />
+    <Route path = "/about" component = { About } />
+    <Route path = "/contact" component = { Contact } />
   </div>
 )
+
+// exact path means you will only render home when you load the page
 
 export default App;
